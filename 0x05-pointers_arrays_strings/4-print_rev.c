f585e84b9c37360fe700baacce572980297b79e0
JaneAbua <abuajane@fjane@gmail.com>
2022-09-20 04:10:31 -0700

    jane

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,21 +6,21 @@
  */
 void print_rev(char *s)
 {
-		int i, j, len;
+	int i, j, len;
 
-		i = 0;
-	
-		while (s[i] != '\0')
-		{
-			i++;
-		}
+	i = 0;
 
-		len = i;
-	
-		for (j = len - 1; j >= 0; j--)
-		{
-			_putchar(s[j]);
-		}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-		_putchar('\n');
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
 }
