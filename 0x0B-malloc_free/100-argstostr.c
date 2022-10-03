e27be5309c9789fbc1bfb88d416842e50a5a5e9e
JaneAbua <abuajane@fjane@gmail.com>
2022-10-03 13:07:28 -0700

    malloc 6

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,3 +24,22 @@ char *argstostr(int ac, char **av)
 	}
 
 	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+
+	return (str);
+}
